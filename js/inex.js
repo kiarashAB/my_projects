9ee99c48dfcd2edd888cd650a4259449b3af0fd8
kiarashAB <kiarash2abdollahi@gmail.com>
2025-07-04 17:49:18 +0330

    test

--- js/inex.js
+++ js/inex.js
@@ -1,10 +1,10 @@
-document.getElementById("Home_Eror").addEventListener("click",()=>{
+document.getElementById("Home_Eror").addEventListener("click", () => {
     // alert("شما اکنون در  صفحه خانه هستی")
-    let erorpage = new Error (" " + "شما در صحفه خانه هستی");
+    let erorpage = new Error(" " + "شما در صحفه خانه هستی");
     alert(erorpage);
 });
 
-document.getElementById("Darkmod").addEventListener("click",()=>{
+document.getElementById("Darkmod").addEventListener("click", () => {
     const body = document.getElementsByTagName("body")[0];
     const moon = document.getElementById("bi-moon");
     const sun = document.getElementById("bi-sun");
@@ -12,4 +12,10 @@ document.getElementById("Darkmod").addEventListener("click",()=>{
     body.classList.toggle("dark");
     moon.classList.toggle("none");
     sun.classList.toggle("none");
+});
+document.getElementById("Le_box").style.cursor = "pointer";
+
+document.getElementById("Le_box").addEventListener("click", () => {
+    document.getElementById("F").classList.toggle("none");
+    document.getElementById("E").classList.toggle("none");
 });
